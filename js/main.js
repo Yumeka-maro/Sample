3c6643415de75ff69b669cfe027e021bb9189365
Yumeka-maro <150841112+Yumeka-maro@users.noreply.github.com>
2023-11-15 04:03:16 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,81 @@
+
+$('.slider').slick({
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  slidesToShow: 3,//スライドを画面に3枚見せる
+  slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
+  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+  dots: true,//下部ドットナビゲーションの表示
+  responsive: [
+    {
+    breakpoint: 769,//モニターの横幅が769px以下の見せ方
+    settings: {
+      slidesToShow: 2,//スライドを画面に2枚見せる
+      slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
+    }
+  },
+  {
+    breakpoint: 426,//モニターの横幅が426px以下の見せ方
+    settings: {
+      slidesToShow: 1,//スライドを画面に1枚見せる
+      slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    }
+  }
+]
+});
+
+// 穴埋め形式です。空いている箇所を埋めてスクーススクロールを実装してください
+$(function () {
+  /*=================================================
+  スムーススクロール
+  ===================================================*/
+  // ページ内のリンクをクリックした時に動作する
+  $('a[href^="#"]').click(function () {
+    // クリックしたaタグのリンクを取得
+    let href = $(this).attr("href");
+    // ジャンプ先のid名をセット hrefの中身が#もしくは空欄なら,htmlタグをセット
+    let target = $(href == "#" || href == "" ? "html" : href);
+    // ページトップからジャンプ先の要素までの距離を取得
+    let position = target.offset().top;
+    // animateでスムーススクロールを行う   ページトップからpositionだけスクロールする
+    // 600はスクロール速度で単位はミリ秒  swingはイージングのひとつ
+    $("html, body").animate({ scrollTop:position}, 600, "swing");
+    // urlが変化しないようにfalseを返す
+    return false;
+  });
+});
+
+  // スクロール時のイベント
+  $(window).scroll(function () {
+    // 画面がスクロールされた時に実行する
+
+    $(".fadein").each(function () {
+      // fadeinクラスに対して順に処理を行う
+      // .each()：個別に処理を行うためのメソッド。繰り返し処理を行いながら各要素に対して操作を実行することができる。
+
+
+      // スクロールした距離
+      let scroll = $(window).scrollTop();
+      // 現在のスクロール位置を取得する。
+      // scrollTop()：要素のスクロール位置を取得
+
+      // fadeinクラスの要素までの距離
+      let target = $(this).offset().top;
+
+      // 画面の高さ
+      let windowHeight = $(window).height();
+
+      // fadeinクラスの要素が画面内にきたタイミングで要素を表示
+      if (scroll > target - windowHeight + 200) {
+
+        // 条件が満たされた場合、要素の不透明度（opacity）を1に設定し、Y軸方向に移動（translateY）させます。
+        $(this).css("opacity", "1");
+        $(this).css("transform", "translateY(0)");
+      }
+    });
+
+  });
+
+
+
